afe88de668aba59bed00fe79e52f6a0d0bab401d
BOSSHIRE202501\bossh <edison.valentino@bosshire.com>
2026-01-15 15:19:43 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const title = document.getElementById("title")
+const runny = document.getElementById("runny")
+const soft = document.getElementById("soft")
+const hard = document.getElementById("hard")
+const container = document.getElementById("container")
+const menu_container = document.getElementById("menu_container")
+const time = document.getElementById("time")
+const reset = document.getElementById("reset")
+
+reset.addEventListener("click", ()=>{
+    resetTimer()
+})
+
+let second = 1
+let intervalId
+
+const runTimer = (interval) =>{
+    second = interval + 1
+    if(!intervalId){
+        intervalId = setInterval(() => {
+            if(second > 0){
+            second--    
+            time.textContent = formatedTime(second)
+            }
+            else{
+                time.textContent = "Your Egg Finished!!!"
+            }
+        }, 1000);
+    }
+        
+}
+
+const formatedTime = (second) =>{
+
+    const formatedSecond = (second % 60) + "'s"
+    const formatedMinute = Math.floor((second / 60)) + "'m"
+
+    return formatedMinute +" "+ formatedSecond
+}
+
+const resetTimer = () =>{
+    clearInterval(intervalId)
+    intervalId = null;
+    time.textContent = ""
+}
+
+
+runny.addEventListener("click", () => {
+    title.textContent = "Runny egg will complete in "
+    runTimer(5*60)
+})
+
+soft.addEventListener("click", () => {
+
+    title.textContent = "Soft egg will complete in "
+    runTimer(7*60)
+})
+
+hard.addEventListener("click", () => {
+
+    title.textContent = "Hard egg will complete in " 
+    runTimer(9*60)
+})
+
