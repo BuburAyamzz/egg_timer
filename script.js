5c5b7dbf7c322f8938877d76e5fb1a406268fbbc
BOSSHIRE202501\bossh <edison.valentino@bosshire.com>
2026-01-15 17:02:17 +0700

    add blipping function

--- script.js
+++ script.js
@@ -6,39 +6,46 @@ const container = document.getElementById("container")
 const menu_container = document.getElementById("menu_container")
 const time = document.getElementById("time")
 const reset = document.getElementById("reset")
+const border = document.getElementById("border")
 
-reset.addEventListener("click", ()=>{
+reset.addEventListener("click", () => {
     resetTimer()
 })
 
 let second = 1
 let intervalId
 
-const runTimer = (interval) =>{
+const runTimer = (interval) => {
     second = interval + 1
-    if(!intervalId){
+    if (!intervalId) {
         intervalId = setInterval(() => {
-            if(second > 0){
-            second--    
-            time.textContent = formatedTime(second)
+            if (second > 0) {
+                second--
+                time.textContent = formatedTime(second)
+                if (second % 2 === 0) {
+                    border.style.backgroundImage = "url(bahlil.jpg)"
+                }
+                else {
+                   border.style.backgroundImage = "url('bahlil_merem.png')";
+                }
             }
-            else{
+            else {
                 time.textContent = "Your Egg Finished!!!"
             }
         }, 1000);
     }
-        
+
 }
 
-const formatedTime = (second) =>{
+const formatedTime = (second) => {
 
     const formatedSecond = (second % 60) + "'s"
     const formatedMinute = Math.floor((second / 60)) + "'m"
 
-    return formatedMinute +" "+ formatedSecond
+    return formatedMinute + " " + formatedSecond
 }
 
-const resetTimer = () =>{
+const resetTimer = () => {
     clearInterval(intervalId)
     intervalId = null;
     time.textContent = ""
@@ -47,18 +54,18 @@ const resetTimer = () =>{
 
 runny.addEventListener("click", () => {
     title.textContent = "Runny egg will complete in "
-    runTimer(5*60)
+    runTimer(5 * 60)
 })
 
 soft.addEventListener("click", () => {
 
     title.textContent = "Soft egg will complete in "
-    runTimer(7*60)
+    runTimer(7 * 60)
 })
 
 hard.addEventListener("click", () => {
 
-    title.textContent = "Hard egg will complete in " 
-    runTimer(9*60)
+    title.textContent = "Hard egg will complete in "
+    runTimer(9 * 60)
 })
 
